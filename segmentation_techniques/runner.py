ae156d579dae5bb728bfa182ea55c2c32039cbe2
tanmaydeshp <tanmaydeshpande19@gmail.com>
2025-01-16 10:17:57 +0100

    Re-evaluated BPE/ULM for english with the correct training dataset

--- segmentation_techniques/runner.py
+++ segmentation_techniques/runner.py
@@ -1,7 +1,12 @@
 import subprocess
-
+import sys
 # Max vocab sizes for BPE and ULM for each language according to sentencepiece error message
 language_data = {
+    "eng": {
+        "bpe": 8000,
+        "ulm": 8000,
+        "min": 1000
+    },
     "ddo": {
         "bpe": 19443,
         "ulm": 5018,
@@ -25,30 +30,30 @@ language_data = {
 }
 
 for language, vocab_sizes in language_data.items():
-    min_vocab_size = vocab_sizes["min"]
-    for model, max_vocab in vocab_sizes.items():
-        if model == "min":
-            continue
-        # Record 35 values until max_vocab
-        intervals = 35
-        step_size = int((max_vocab - min_vocab_size) / intervals)
+        min_vocab_size = vocab_sizes["min"]
+        for model, max_vocab in vocab_sizes.items():
+            if model == "min":
+                continue
+            # Record 35 values until max_vocab
+            intervals = 35
+            step_size = int((max_vocab - min_vocab_size) / intervals)
 
-        # All values, including max_vocab explicitely
-        vocabs = [round(min_vocab_size + step_size * i) for i in range(intervals)] + [max_vocab]
-        vocabs = sorted(set(vocabs))
+            # All values, including max_vocab explicitely
+            vocabs = [round(min_vocab_size + step_size * i) for i in range(intervals)] + [max_vocab]
+            vocabs = sorted(set(vocabs))
 
-        for vocab_size in vocabs:
-            try:
-                result = subprocess.run(
-                    ["python", "record_eval.py", 
-                     "--model",     model, 
-                     "--language",  language, 
-                     "--size",      str(vocab_size), 
-                     "--train",     f"data/{language}.train.txt", 
-                     "--test",      f"data/{language}.test.gold.tsv", 
-                     "--gold",      f"data/{language}.test.gold.tsv", 
-                     "--guess",     f"outputs/{language}.sentence.test.{model}_guess.tsv", 
-                     "--output",    f"outputs/{language}.output.json"]
-                )
-            except subprocess.CalledProcessError as e:
-                print(f"Error occurred: {e.stderr}")
+            for vocab_size in vocabs:
+                try:
+                    result = subprocess.run(
+                        [sys.executable, "record_eval.py", 
+                        "--model",     model, 
+                        "--language",  language, 
+                        "--size",      str(vocab_size), 
+                        "--train",     f"data/{language}.train.txt", 
+                        "--test",      f"data/{language}.test.gold.tsv", 
+                        "--gold",      f"data/{language}.test.gold.tsv", 
+                        "--guess",     f"outputs/{language}.sentence.test.{model}_guess.tsv", 
+                        "--output",    f"outputs/{language}.output.json"]
+                    )
+                except subprocess.CalledProcessError as e:
+                    print(f"Error occurred: {e.stderr}")
